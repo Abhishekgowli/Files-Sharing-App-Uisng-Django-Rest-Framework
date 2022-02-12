a771b7e56d2358a2f9b4f3bc98a402b07b9645cd
abhishekgowli <gowlabhi@gmial.com>
2022-02-12 09:14:45 +0530

    fix/zip

--- home/models.py
+++ home/models.py
@@ -1,17 +1,18 @@
-from msilib.schema import Class
 from statistics import mode
 from tkinter import CASCADE
 from django.db import models
+from uuid import uuid4
 import uuid
 import os
 # Create your models here.
 class Folder(models.Model):
-    uid=models.UUIDField(primary_key=True,editable=False,default=uuid.uuid4.uuid)
+    uid=models.UUIDField(primary_key=True,editable=False,default=uuid.uuid4)
     created_at=models.DateField(auto_now=True)
 
 def get_upload_path(instance,filename):
     return os.path.join(str(instance.folder.uid),filename)
+
 class Files(models.Model):
     folder=models.ForeignKey(Folder,on_delete=models.CASCADE)
-    file=models.FileField(upload_to='')
+    file=models.FileField(upload_to=get_upload_path)
     created_at=models.DateField(auto_now=True)
--- home/views.py
+++ home/views.py
@@ -1,5 +1,33 @@
+from urllib import response
 from django.shortcuts import render
+from rest_framework.views import APIView
+from .serializers import FileListSeralizer
+from rest_framework.response import Response
 
 # Create your views here.
+
+class HandelFileUpload(APIView):
+    def post(self,request):
+        try:
+            data=request.data
+            serializer=FileListSeralizer(data=data)
+            
+            if serializer.is_valid():
+                serializer.save()
+                return Response({
+                    'status':200,
+                    'message':'files uploaded successfully'
+                })
+            return Response({
+                    'status':400,
+                    'message':'someting went wrong',
+                    'data':serializer.errors
+                })
+        except Exception as e:
+            print(e)
+
+
+
+
 def home(request):
     return render(request,'home.html')
